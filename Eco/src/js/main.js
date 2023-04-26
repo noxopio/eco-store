e80d43cbc729e24117ba18e9bcd796d5d40b23d8
nox <dark.tempus@gmail.com>
2023-04-25 22:17:52 -0500

    loaging

--- Eco/src/js/main.js
+++ Eco/src/js/main.js
@@ -1,29 +1,47 @@
 'use strict'
 
-const gallery    = document.querySelector('.gallery-img')
-const punto     = document.querySelectorAll('.punto')
+
+
+const overlay = document.querySelector("#overlay");
+const body = document.querySelector("body");
+const gallery = document.querySelector('.gallery-img')
+const punto = document.querySelectorAll('.punto')
+
+window.onload = (event) => {
+    setTimeout(() => {
+     overlay.addEventListener('click', of)
+    function of() {
+        overlay.style.display = "none";
+        body.classList.remove("hidden");
+    };
+
+        
+    },4000);
+  };
+
+
 
 // Cuando CLICK en punto
-    // Saber la posición de ese punto
-    // Aplicar un transform translateX al gallery
-    // QUITAR la clase activo de TODOS puntos
-    // AÑADIR la clase activo al punto que hemos hecho CLICK
+// Saber la posición de ese punto
+// Aplicar un transform translateX al gallery
+// QUITAR la clase activo de TODOS puntos
+// AÑADIR la clase activo al punto que hemos hecho CLICK
 
 // Recorrer TODOS los punto
-punto.forEach( ( cadaPunto , i )=> {
+punto.forEach((cadaPunto, i) => {
     // Asignamos un CLICK a cadaPunto
-    punto[i].addEventListener('click',()=>{
+    punto[i].addEventListener('click', () => {
 
         // Guardar la posición de ese PUNTO
-        let posicion  = i
+        let posicion = i
         // Calculando el espacio que debe DESPLAZARSE el GRANDE
         let operacion = posicion * -33.3;
 
         // MOVEMOS el grand
-        gallery.style.transform = `translateX(${ operacion }%)`
+        gallery.style.transform = `translateX(${operacion}%)`
 
         // Recorremos TODOS los punto
-        punto.forEach( ( cadaPunto , i )=>{
+        punto.forEach((cadaPunto, i) => {
             // Quitamos la clase ACTIVO a TODOS los punto
             punto[i].classList.remove('activo')
         })
@@ -32,3 +50,4 @@ punto.forEach( ( cadaPunto , i )=> {
 
     })
 })
+
